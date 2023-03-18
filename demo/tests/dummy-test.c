ffb1615756d42cd605431a08861cfa7701ebdfe0
Eli Wald <eliwald@stanford.edu>
2023-03-18 13:55:53 -0700

    fix dereference bug in dummy-test

--- demo/tests/dummy-test.c
+++ demo/tests/dummy-test.c
@@ -4,28 +4,36 @@
 #include "nrf-hw-support.h"
 #include "esp-helpers.h"
 
-enum {GET_MAC_ADDRESS = 0x65};
+enum {GET_MAC_ADDRESS = 0x65,
+GET_WIFI_MODE = 0x67};
 
 void notmain(void) {
   spi_t spi_if = pin_init(8, 0);
 
   uint8_t mac = GET_MAC_ADDRESS;
+  uint8_t w_mode = GET_WIFI_MODE;
 
   // TODO: create buf for TLV structure, use `compose_tlv` helper to build
   //
   char buf[1024];
   char res[1024];
+  memset(buf, 0, sizeof(buf));
+  memset(res, 0, sizeof(res));
   compose_tlv(buf, &mac, 1);
   printk("Composed TLV\n");
+  printk("-----Current TLV buffer----\n");
+  for (int i = 0; i < sizeof(buf) / sizeof(uint32_t); i++) {
+    printk("%x", buf[i*4]);
+  }
+  printk("\n---------------------------\n");
   while (1) {
-
-    spi_n_transfer(spi_if, &mac, res, 1024);
-    if (res != 0) {
+    spi_n_transfer(spi_if, &mac, res, 4);
+    if (*res != 0) {
       printk("Got response: \n");
       break;
     }
   }
-  for (int i = 0; i < 1024 / sizeof(uint32_t); i++) {
+  for (int i = 0; i < sizeof(res) / sizeof(uint32_t); i++) {
     printk("%x", res[i*4]);
   }
   printk("\n");
